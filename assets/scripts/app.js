ce0557f533ffee84a83e440dcb0db0d4245f40fa
Erudite885 <erudite885@gmail.com>
2023-01-05 07:51:49 +0100

    converting data types

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,17 @@
-alert("this works");
+// very basic calculator
+let currentResult = 0;
+let calculationDescription;
+
+function add() {
+  currentResult = currentResult + userInput.value;
+  calculationDescription = "Description";
+  outputResult(currentResult, calculationDescription);
+  //   alert(`The result is ${result}`);
+  // return result;
+}
+
+// const additionResult =  add(25, 55);
+// add(21, 89);
+// currentResult = additionResult;
+
+addBtn.addEventListener("click", add);
